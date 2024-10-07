c091bcdf050193575ee04f0d6835c80a1c661fc5
Chrystian Shiruo <chrystian.shiruo@gmail.com>
2024-10-06 22:52:09 -0300

    Pass card related code from UIController to CardsManager;

---
+++ SucoDeLaranja/Assets/Scripts/CardsManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardsManager : MonoBehaviour {
+
+    public static float cardScaleMultiplier;
+
+    [SerializeField] private GameObject _cardPrefab;
+    [SerializeField] private RectTransform _boardPanel;
+
+    private GameData _gameData;
+    private GridLayoutGroup _boardPanelLayoutGroup;
+
+    public void Init(GameData gameData) {
+        _gameData = gameData;
+
+        _boardPanelLayoutGroup = _boardPanel.GetComponent<GridLayoutGroup>();
+        _boardPanelLayoutGroup.constraintCount = _gameData.Board.GetLength(1);
+
+        SetBoardLayout(_boardPanel, new Vector2Int(_gameData.Board.GetLength(0), _gameData.Board.GetLength(1)));
+        InstantiateCards(_gameData.Board);
+
+    }
+
+
+    private void InstantiateCards(CardState[,] board) {
+
+        int width = board.GetLength(0);
+        int height = board.GetLength(1);
+
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                GameObject go = Instantiate(_cardPrefab, _boardPanel);
+                go.GetComponent<Card>().Init(board[x, y]);
+            }
+        }
+    }
+
+    private void SetBoardLayout(RectTransform boardPanel, Vector2Int dimensions) {
+        float x = boardPanel.rect.width;
+        float y = boardPanel.rect.height;
+        Debug.Log($"{x}, {y}");
+        x = x / dimensions.x;
+        y = y / dimensions.y;
+        cardScaleMultiplier = (x > y ? y : x) / 100f;
+        _boardPanelLayoutGroup.cellSize = new Vector2(x, y);
+    }
+}
--- SucoDeLaranja/Assets/Scripts/UIController.cs
+++ SucoDeLaranja/Assets/Scripts/UIController.cs
@@ -5,12 +5,7 @@ using TMPro;
 
 public class UIController : MonoBehaviour {
 
-    public static float cardScaleMultiplier;
 
-
-    [Header("Board")]
-    [SerializeField] private GameObject _cardPrefab;
-    [SerializeField] private RectTransform _boardPanel;
 
     [Header("Counters")]
     [SerializeField] private TextMeshProUGUI _scoreText;
@@ -19,16 +14,10 @@ public class UIController : MonoBehaviour {
 
     private int pairs;
     private GameData _gameData;
-    private GridLayoutGroup _boardPanelLayoutGroup;
-
 
     public void Init(GameData gameData) {
         _gameData = gameData;
-        _boardPanelLayoutGroup = _boardPanel.GetComponent<GridLayoutGroup>();
-        _boardPanelLayoutGroup.constraintCount = _gameData.Board.GetLength(1);
 
-        SetBoardLayout(_boardPanel, new Vector2Int(_gameData.Board.GetLength(0), _gameData.Board.GetLength(1)));
-        InstantiateCards(_gameData.Board);
 
         pairs = (_gameData.Board.GetLength(0) * _gameData.Board.GetLength(1)) / 2;
         _gameData.OnScoreChange += SetScoreText;
@@ -60,30 +49,6 @@ public class UIController : MonoBehaviour {
         SetScoreText(gameData.Score);
         SetMatchesText(gameData.Matches);
         SetTurnsText(gameData.Turns);
-    }
-
-
-    private void SetBoardLayout(RectTransform boardPanel, Vector2Int dimensions) {
-        float x = boardPanel.rect.width;
-        float y = boardPanel.rect.height;
-        Debug.Log($"{x}, {y}");
-        x = x / dimensions.x;
-        y = y / dimensions.y;
-        cardScaleMultiplier = (x > y ? y : x) / 100f;
-        _boardPanelLayoutGroup.cellSize = new Vector2(x, y);
-    }
-
-    private void InstantiateCards(CardState[,] board) {
-
-        int width = board.GetLength(0);
-        int height = board.GetLength(1);
-
-        for(int x = 0; x < width; x++) {
-            for(int y = 0; y < height; y++) {
-                GameObject go = Instantiate(_cardPrefab, _boardPanel);
-                go.GetComponent<Card>().Init(board[x, y]);
-            }
-        }
     }
 
     private void OnDestroy() {
