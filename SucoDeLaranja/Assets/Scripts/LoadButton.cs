924e68a95a034f9478c40ee11eb103384b4c7652
Chrystian Shiruo <chrystian.shiruo@gmail.com>
2024-10-08 09:57:10 -0300

    Empty save file handling;

---
+++ SucoDeLaranja/Assets/Scripts/LoadButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadButton : MonoBehaviour {
+
+    public static Action InteractableWaitQueue;
+
+    private Button _button;
+
+    private void Awake() {
+        _button = GetComponent<Button>();
+        _button.interactable = false;
+    }
+    private void Start() {
+        CheckIfDataExists();
+        DataManager.OnSaveGameData += CheckIfDataExists;
+    }
+
+    private void CheckIfDataExists() {
+        _button.interactable = DataManager.SaveExists();
+    }
+
+    private void OnDestroy() {
+        DataManager.OnSaveGameData -= CheckIfDataExists;
+    }
+}
+
